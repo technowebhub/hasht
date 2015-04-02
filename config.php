9ff08eebcd8f0ca91a959889ff35b77f5f9deb69
Alvin Reyes <alvin.jay.p.reyes@gmail.com>
2015-04-02 17:22:36 +0800

    Commited the config.php file for reference

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('Philippines/Manila');
+
+$twitter = array(
+    'consumer_key' =>           '<generated consumer key>',
+    'consumer_secret' =>        '<generated consumer secret>',
+    'access_token' =>           '<generated access token>',
+    'access_token_secret' =>    '<generated access token secret>'
+);
+
+$instagram = array(
+    'client_id' =>       '<generated client>',
+    'client_secret' =>  '<generated client secret>',
+    'website_url' =>    '<url of site>/instagram',
+    'redirect_uri' =>   '<url of site>/instagram',
+    'access_token' =>   '<generated access token>'
+);
+
+$db = array(
+    'host' =>       'localhost',
+    'user' =>       '<db user>',
+    'password' =>   '<db password>',
+    'name' =>       '<database name>'
+);
+
+$hashtag = 'heyjayforever';
+
+?>
